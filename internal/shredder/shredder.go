53af0cc4db2b6868e1b4ab23e62f9d98a7bf67b1
Donald Ball <donald.ball@gmail.com>
2022-11-14 17:00:14 -0500

    add assert, check for struct

--- internal/shredder/shredder.go
+++ internal/shredder/shredder.go
@@ -2,6 +2,7 @@
 package shredder
 
 import (
+	"errors"
 	"reflect"
 	"strconv"
 	"time"
@@ -13,6 +14,7 @@ import (
 // Shredder shreds structs into claims.
 type Shredder interface {
 	Retract(x any) (req Request, err error)
+	Assert(x any) (req Request, err error)
 }
 
 // shredder is a stateful shredder.
@@ -27,12 +29,15 @@ func (s *shredder) nextTempID() TempID {
 }
 
 func (s *shredder) Retract(x any) (req Request, err error) {
-	tempid := s.nextTempID()
-	req.Claims = []*Claim{{E: tempid, Retract: true}}
+	e := s.nextTempID()
+	req.Claims = []*Claim{{E: e, Retract: true}}
 	tempidConstraints := map[IDRef]Void{}
-	req.TempIDs = map[TempID]map[IDRef]Void{tempid: tempidConstraints}
+	req.TempIDs = map[TempID]map[IDRef]Void{e: tempidConstraints}
 	typ := reflect.TypeOf(x)
-	// TODO return an error instead of panicking if x is not a struct
+	if typ.Kind() != reflect.Struct {
+		err = errors.New("invalid type")
+		return
+	}
 	n := typ.NumField()
 	for i := 0; i < n; i++ {
 		fieldType := typ.Field(i)
@@ -49,36 +54,109 @@ func (s *shredder) Retract(x any) (req Request, err error) {
 				// TODO error?
 			}
 			continue
-		} else if attr.Unique != 0 {
-			// TODO extract into helper fn
-			var vref VRef
+		}
+		if attr.Unique == 0 {
+			continue
+		}
+		// TODO extract into helper fn
+		var vref VRef
+		switch fieldType.Type.Kind() {
+		case reflect.Bool:
+			vref = Bool(fieldValue.Bool())
+		case reflect.Int:
+			vref = Int(fieldValue.Int())
+		case reflect.String:
+			vref = String(fieldValue.String())
+		case reflect.Struct:
+			v := fieldValue.Interface()
+			switch typed := v.(type) {
+			case time.Time:
+				vref = Inst(typed)
+			default:
+				// TODO recurse, but that probably means we need to pass along the req?
+			}
+		case reflect.Float64:
+			vref = Float(fieldValue.Float())
+		default:
+			// TODO error?
+			continue
+		}
+		v, ok := vref.(Value)
+		if !ok {
+			// TODO panic or err or
+			continue
+		}
+		// TODO the isEmpty control bit might be a flag on the struct field
+		if !v.IsEmpty() {
+			tempidConstraints[LookupRef{A: attr.Ident, V: v}] = Void{}
+		}
+	}
+	return
+}
+
+func (s *shredder) Assert(x any) (req Request, err error) {
+	e := s.nextTempID()
+	tempidConstraints := map[IDRef]Void{}
+	req.TempIDs = map[TempID]map[IDRef]Void{e: tempidConstraints}
+	typ := reflect.TypeOf(x)
+	if typ.Kind() != reflect.Struct {
+		err = errors.New("invalid type")
+		return
+	}
+	n := typ.NumField()
+	req.Claims = make([]*Claim, 0, n)
+	for i := 0; i < n; i++ {
+		fieldType := typ.Field(i)
+		attr := parseAttrField(fieldType)
+		if attr.Ident == "" {
+			continue
+		}
+		fieldValue := reflect.ValueOf(x).Field(i)
+		if attr.Ident == sys.DbId {
 			switch fieldType.Type.Kind() {
-			case reflect.Bool:
-				vref = Bool(fieldValue.Bool())
-			case reflect.Int:
-				vref = Int(fieldValue.Int())
-			case reflect.String:
-				vref = String(fieldValue.String())
-			case reflect.Struct:
-				v := fieldValue.Interface()
-				switch typed := v.(type) {
-				case time.Time:
-					vref = Inst(typed)
-				default:
-					// TODO recurse, but that probably means we need to pass along the req?
-				}
-			case reflect.Float64:
-				vref = Float(fieldValue.Float())
+			case reflect.Uint:
+				tempidConstraints[ID(fieldValue.Uint())] = Void{}
 			default:
 				// TODO error?
-				continue
 			}
-			v, ok := vref.(Value)
-			// TODO the isEmpty control bit might be a flag on the struct field
-			if ok && !v.IsEmpty() {
-				tempidConstraints[LookupRef{A: attr.Ident, V: v}] = Void{}
-			}
+			continue
 		}
+		// TODO extract into helper fn
+		var vref VRef
+		switch fieldType.Type.Kind() {
+		case reflect.Bool:
+			vref = Bool(fieldValue.Bool())
+		case reflect.Int:
+			vref = Int(fieldValue.Int())
+		case reflect.String:
+			vref = String(fieldValue.String())
+		case reflect.Struct:
+			v := fieldValue.Interface()
+			switch typed := v.(type) {
+			case time.Time:
+				vref = Inst(typed)
+			default:
+				// TODO recurse, but that probably means we need to pass along the req?
+			}
+		case reflect.Float64:
+			vref = Float(fieldValue.Float())
+		default:
+			// TODO error?
+			continue
+		}
+		v, ok := vref.(Value)
+		if !ok {
+			// TODO panic or err or
+			continue
+		}
+		// TODO the isEmpty control bit might be a flag on the struct field
+		if v.IsEmpty() {
+			continue
+		}
+		if attr.Unique != 0 {
+			tempidConstraints[LookupRef{A: attr.Ident, V: v}] = Void{}
+		}
+		req.Claims = append(req.Claims, &Claim{E: e, A: attr.Ident, V: vref})
 	}
 	return
 }
--- internal/shredder/shredder_test.go
+++ internal/shredder/shredder_test.go
@@ -8,24 +8,53 @@ import (
 )
 
 func TestShred(t *testing.T) {
-	shredder := NewShredder()
 	type person struct {
 		id   uint   `attr:"sys/db/id"`
 		name string `attr:"person/name,unique"`
 		uuid string `attr:"person/uuid,identity"`
 		age  int    `attr:"person/age"`
 	}
-	txn, err := shredder.Retract(person{id: 23, name: "Donald", age: 48})
-	assert.NoError(t, err)
 
-	expected := Request{
-		Claims: []*Claim{{E: TempID("1"), A: nil, V: nil, Retract: true}},
-		TempIDs: map[TempID]map[IDRef]Void{
-			TempID("1"): {
-				ID(23): Void{},
-				LookupRef{A: Ident("person/name"), V: String("Donald")}: Void{},
+	t.Run("Retract", func(t *testing.T) {
+		shredder := NewShredder()
+		txn, err := shredder.Retract(person{id: 23, name: "Donald", age: 48})
+		assert.NoError(t, err)
+
+		expected := Request{
+			Claims: []*Claim{{E: TempID("1"), A: nil, V: nil, Retract: true}},
+			TempIDs: map[TempID]map[IDRef]Void{
+				TempID("1"): {
+					ID(23): Void{},
+					LookupRef{A: Ident("person/name"), V: String("Donald")}: Void{},
+				},
 			},
-		},
-	}
-	assert.Equal(t, expected, txn)
+		}
+		assert.Equal(t, expected, txn)
+	})
+
+	t.Run("Assert", func(t *testing.T) {
+		shredder := NewShredder()
+		txn, err := shredder.Assert(person{id: 23, name: "Donald", age: 48})
+		assert.NoError(t, err)
+
+		expected := Request{
+			Claims: []*Claim{
+				{E: TempID("1"), A: Ident("person/name"), V: String("Donald")},
+				{E: TempID("1"), A: Ident("person/age"), V: Int(48)},
+			},
+			TempIDs: map[TempID]map[IDRef]Void{
+				TempID("1"): {
+					ID(23): Void{},
+					LookupRef{A: Ident("person/name"), V: String("Donald")}: Void{},
+				},
+			},
+		}
+		assert.Equal(t, expected, txn)
+	})
+
+	t.Run("invalid values", func(t *testing.T) {
+		shredder := NewShredder()
+		_, err := shredder.Assert(5)
+		assert.Error(t, err)
+	})
 }
