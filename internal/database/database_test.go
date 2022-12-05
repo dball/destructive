d86e1a6a50cb0b5e942298e92f3b563205a07701
Donald Ball <donald.ball@gmail.com>
2022-12-04 20:54:41 -0500

    specify write attribute

--- internal/database/database_test.go
+++ internal/database/database_test.go
@@ -22,3 +22,32 @@ func TestWriteSimple(t *testing.T) {
 	assert.Equal(t, map[TempID]ID{TempID("1"): sys.FirstUserID + 1}, res.NewIDs)
 	assert.NotNil(t, res.Snapshot)
 }
+
+func TestWriteAttr(t *testing.T) {
+	db := NewIndexDatabase(32, 64, 64)
+	req := Request{
+		Claims: []*Claim{
+			{E: TempID("1"), A: sys.DbIdent, V: String("person/name")},
+			{E: TempID("1"), A: sys.AttrType, V: sys.AttrTypeString},
+			{E: TempID("1"), A: sys.AttrUnique, V: sys.AttrUniqueIdentity},
+		},
+		TempIDs: map[TempID]map[IDRef]Void{},
+	}
+	res := db.Write(req)
+	assert.NoError(t, res.Error)
+	assert.Positive(t, res.ID)
+	assert.Equal(t, map[TempID]ID{TempID("1"): sys.FirstUserID + 1}, res.NewIDs)
+	assert.NotNil(t, res.Snapshot)
+
+	req = Request{
+		Claims: []*Claim{
+			{E: TempID("1"), A: Ident("person/name"), V: String("Donald")},
+		},
+		TempIDs: map[TempID]map[IDRef]Void{},
+	}
+	res = db.Write(req)
+	assert.NoError(t, res.Error)
+	assert.Zero(t, res.ID)
+	assert.Nil(t, res.NewIDs)
+	assert.NotNil(t, res.Snapshot)
+}
