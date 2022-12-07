ea1063db8c3f75dbc121076b6569bf730f65a3fa
Donald Ball <donald.ball@gmail.com>
2022-12-06 21:25:01 -0500

    specify time is not indexed or compared

--- internal/index/index_test.go
+++ internal/index/index_test.go
@@ -8,15 +8,23 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func newAllocator() func() ID {
+	nextID := sys.FirstUserID
+	return func() (id ID) {
+		id = nextID
+		nextID++
+		return
+	}
+}
+
 func TestIndex(t *testing.T) {
-	id := sys.FirstUserID
-	a := id
-	id++
+	allocate := newAllocator()
+	a := allocate()
 	idx := NewCompositeIndex(32, EAVIndex, map[ID]ID{a: sys.AttrTypeString})
-	e := id
-	id++
-	d1 := Datum{E: e, A: a, V: String("donald"), T: e}
-	d2 := Datum{E: e, A: a, V: String("Donald"), T: e}
+	e := allocate()
+	tx := allocate()
+	d1 := Datum{E: e, A: a, V: String("donald"), T: tx}
+	d2 := Datum{E: e, A: a, V: String("Donald"), T: tx}
 
 	assert.False(t, idx.Insert(d1))
 	assert.True(t, idx.Insert(d1))
@@ -41,4 +49,23 @@ func TestIndex(t *testing.T) {
 	assert.True(t, idx.Delete(d2))
 	assert.False(t, idx.Find(d2))
 	assert.True(t, clone.Find(d2))
+}
+
+func TestIndexTimeProperties(t *testing.T) {
+	allocate := newAllocator()
+	a := allocate()
+	idx := NewCompositeIndex(32, EAVIndex, map[ID]ID{a: sys.AttrTypeString})
+	e := allocate()
+	t1 := allocate()
+	t2 := allocate()
+	d1 := Datum{E: e, A: a, V: String("Donald"), T: t1}
+	d2 := Datum{E: e, A: a, V: String("Donald"), T: t2}
+
+	assert.False(t, idx.Insert(d1))
+
+	assert.Equal(t, []Datum{d1}, idx.Select(EA, d2).Drain())
+	assert.True(t, idx.Find(d2))
+	assert.True(t, idx.Delete(d2))
+	assert.False(t, idx.Delete(d2))
+	assert.False(t, idx.Find(d2))
 }
