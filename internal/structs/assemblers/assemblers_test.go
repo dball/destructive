80ec85f4cc07b1de0ffb57bfb54325c287a42e05
Donald Ball <donald.ball@gmail.com>
2022-12-22 11:06:47 -0500

    more assemblers test

--- internal/structs/assemblers/assemblers_test.go
+++ internal/structs/assemblers/assemblers_test.go
@@ -31,10 +31,12 @@ func TestSimple(t *testing.T) {
 		analyzer := models.BuildCachingAnalyzer()
 		database.Declare(db,
 			Attr{Ident: "person/name", Type: sys.AttrTypeString},
+			Attr{Ident: "person/title", Type: sys.AttrTypeString},
 		)
 		req := Request{
 			Claims: []*Claim{
 				{E: TempID("1"), A: Ident("person/name"), V: String("Donald")},
+				{E: TempID("1"), A: Ident("person/title"), V: String("Citizen")},
 			},
 		}
 		res := db.Write(req)
@@ -43,6 +45,8 @@ func TestSimple(t *testing.T) {
 		assembler := NewAssembler(analyzer, res.Snapshot)
 		entity, err := Assemble(assembler, id, (*Person)(nil))
 		assert.NoError(t, err)
-		assert.Equal(t, Person{ID: uint64(id), Name: "Donald"}, entity)
+		title := "Citizen"
+		expected := Person{ID: uint64(id), Name: "Donald", Title: &title}
+		assert.Equal(t, expected, entity)
 	})
 }
